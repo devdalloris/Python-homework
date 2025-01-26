a7892e7e6ef02692181b42d9d95dd31e3513b3a6
dalloris <dalloris.uz@gmail.com>
2025-01-26 23:37:30 +0500

    ..

--- lesson-18/homework/practice.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-data = [
-    ['John', 'Doe', 40],
-    ['Jone', 'Doe', 44],
-    ['Adam', 'Smith', 40]
-]
-columns=['Firtname', 'Lastname', 'Age']
-df=pd.DataFrame(data=data, columns=columns)
-print(df)
